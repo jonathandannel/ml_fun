5b6133416929aea97fc5b10b03234f15a82aa04d
jonathan dannel <usixjad@gmail.com>
2020-03-31 22:49:10 -0400

    plot how ransac calculates outliers/inliers

--- robust_regression.py
+++ robust_regression.py
@@ -22,15 +22,41 @@ RANSAC.fit(X, y)
 inlier_mask = RANSAC.inlier_mask_
 outlier_mask = np.logical_not(inlier_mask)
 
+line_X = np.arange(3, 10, 1)
+# [3, 4, 5, 6, 7, 8, 9] rooms
+
+line_y_ransac = RANSAC.predict(line_X.reshape(-1, 1))
+
 def test():
     # print(inlier_mask)
     # print(outlier_mask)
     # boston.get_corr_heatmap(['MEDV', 'RM'])
     boston.get_regplot(X, y)
 
-test()
+# Let's illustrate what happened
+def plot():
+    sns.set(style="darkgrid")
+    plt.figure(figsize=(12, 8))
+    plt.xlabel("rooms")
+    plt.ylabel("medv")
+    plt.legend(loc="upper left")
 
+    print(line_y_ransac)
 
+    # X = df['RM'].values.reshape(-1, 1)
+    # y = df['MEDV'].values 
+    
+    plt.scatter(X[inlier_mask], y[inlier_mask],
+                c="blue", marker="o", label="inliers")
+    plt.scatter(X[outlier_mask], y[outlier_mask],
+                c="brown", marker="s", label="outliers")
+    
+    # Predicted value
+    plt.plot(line_X, line_y_ransac, color="red")
+    
+    plt.show()
+
+plot()
 
 
 
